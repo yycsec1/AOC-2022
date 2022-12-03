dd6c3f40c904a8d047632167efc9a84d29d51d99
yycsec <github@devsec.one>
2022-12-03 00:21:12 -0700

    Solution for day 3.

--- day3/day3.go
+++ day3/day3.go
@@ -7,64 +7,40 @@ import (
 	"unicode"
 )
 
-type Rucksack struct {
-	compOne, compTwo string
-}
-
-type Group struct {
-	rucksacks [3]string
-	indexes   [3]int
-}
-
-func parseInputOne(fileName string) (rucksacks []Rucksack) {
-	byteData, _ := os.ReadFile(fileName)
-
-	for _, line := range strings.Split(string(byteData), "\r\n") {
-		half := len(line) / 2
-		rucksacks = append(rucksacks, Rucksack{line[:half], line[half:]})
-	}
-
-	return
-}
-
-func parseInputTwo(fileName string) (groups []Group) {
+func parseInput(fileName string) (rucksacks [][]string, groups [][]string) {
 	byteData, _ := os.ReadFile(fileName)
 
 	stringData := strings.Split(string(byteData), "\r\n")
 	for i := 0; i < len(stringData); i += 3 {
-		indexes := [3]int{0, 1, 2}
 
-		if len(stringData[i+1]) < len(stringData[i+2]) {
-			if len(stringData[i+1]) < len(stringData[i]) {
-				indexes = [3]int{1, 0, 2}
-			}
-		} else {
-			if len(stringData[i+2]) < len(stringData[i]) {
-				indexes = [3]int{2, 0, 1}
-			}
-		}
-
-		groups = append(groups, Group{[3]string{stringData[i], stringData[i+1], stringData[i+2]}, indexes})
+		rucksacks = append(rucksacks,
+			[]string{stringData[i][:len(stringData[i])/2], stringData[i][len(stringData[i])/2:]},
+			[]string{stringData[i+1][:len(stringData[i+1])/2], stringData[i+1][len(stringData[i+1])/2:]},
+			[]string{stringData[i+2][:len(stringData[i+2])/2], stringData[i+2][len(stringData[i+2])/2:]},
+		)
+		groups = append(groups, []string{stringData[i], stringData[i+1], stringData[i+2]})
 	}
-
 	return
+}
+
+func score(ch rune) int {
+	if unicode.IsLower(ch) {
+		return int(ch) - 96
+	}
+	return int(ch) - 38
 }
 
 func Solve1() {
 	priorities := 0
-	rucksacks := parseInputOne("./day3/input.txt")
+	rucksacks, _ := parseInput("./day3/input.txt")
 
 	for _, rucksack := range rucksacks {
-		for _, ch := range rucksack.compOne {
-			if !strings.ContainsRune(rucksack.compTwo, ch) {
+		for _, ch := range rucksack[0] {
+			if !strings.ContainsRune(rucksack[1], ch) {
 				continue
 			}
 
-			if unicode.IsLower(ch) {
-				priorities += int(ch) - 96
-				break
-			}
-			priorities += int(ch) - 38
+			priorities += score(ch)
 			break
 
 		}
@@ -75,21 +51,16 @@ func Solve1() {
 
 func Solve2() {
 	priorities := 0
-	groups := parseInputTwo("./day3/input.txt")
+	_, groups := parseInput("./day3/input.txt")
 
 	for _, group := range groups {
-		for _, ch := range group.rucksacks[group.indexes[0]] {
-			if !(strings.ContainsRune(group.rucksacks[group.indexes[1]], ch) && strings.ContainsRune(group.rucksacks[group.indexes[2]], ch)) {
+		for _, ch := range group[0] {
+			if !(strings.ContainsRune(group[1], ch) && strings.ContainsRune(group[2], ch)) {
 				continue
 			}
 
-			if unicode.IsLower(ch) {
-				priorities += int(ch) - 96
-				break
-			}
-			priorities += int(ch) - 38
+			priorities += score(ch)
 			break
-
 		}
 	}
 
