dc7d23b9f76e131873c1cf85420ef5b279842d1e
yycsec <github@devsec.one>
2022-12-05 22:41:29 -0700

    Solution for day 6.

---
+++ day6/day6.go
@@ -0,0 +1,42 @@
+package day6
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Solve1() {
+	buffer := parseInput("./day6/input.txt")
+	for i := 0; i < len(buffer); i++ {
+		if isStartMarker(buffer[i : i+4]) {
+			fmt.Printf("The result is %d\n", i+4)
+			break
+		}
+	}
+}
+
+func Solve2() {
+	buffer := parseInput("./day6/input.txt")
+	for i := 0; i < len(buffer); i++ {
+		if isStartMarker(buffer[i : i+14]) {
+			fmt.Printf("The result is %d\n", i+14)
+			break
+		}
+	}
+}
+
+func parseInput(fileName string) (input string) {
+	byteData, _ := os.ReadFile(fileName)
+	input = string(byteData)
+	return
+}
+
+func isStartMarker(str string) bool {
+	for _, ch := range str {
+		if strings.Count(str, string(ch)) > 1 {
+			return false
+		}
+	}
+	return true
+}
